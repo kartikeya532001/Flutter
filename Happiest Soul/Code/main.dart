581bd6c79bc984d0d1e1fe3abbddc32f2db1fd44
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2020-08-30 03:52:45 +0700

    Main file

---
+++ Happiest Soul/Code/main.dart
@@ -0,0 +1,16 @@
+import 'package:Happiest_Soul/Code/Home.dart';
+import 'package:Happiest_Soul/Code/Profile.dart';
+import 'package:flutter/material.dart';
+import 'Code/Login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: "/",
+    routes: {
+      "/": (context) => Mylogin(),
+      "/home": (context) => MyHome(),
+      "/profile": (context) => MyProfile(),
+    },
+  ));
+}
